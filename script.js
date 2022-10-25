544d1c951d9519d91a8697d15ebadda884aa3aa7
Belskaya20 <110544821+Belskaya20@users.noreply.github.com>
2022-10-25 15:07:50 +0300

    Домашнее задание 11

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-const spans = document.querySelectorAll('.word');
+const word = document.querySelectorAll('.word');
 const timer = document.getElementById('timer');
+const wrongCount = document.querySelector('.wrong-count');
+const wordMistake = document.querySelector('.word-mistakes');
+const correctCount = document.querySelector('.correct-count');
 
-function random () {
+function random() {
     const words = [
         'mother', 'world', 'work', 'friend', 'book', 'idea',
         'cat', 'mountain', 'fish', 'letter', 'girl', 'boy',
@@ -10,18 +13,34 @@ function random () {
         'pen', 'phone', 'son', 'travel', 'six', 'morning'
     ]
 
-    const randWord = Math.floor(Math.random()*(words.length));
-    spans.innerHTML = words;
+    const randWord = Math.floor(Math.random() * (words.length));
     return words[randWord];
 }
 
 
+function insertWord(words) {
+    word.innerHTML = words;
+}
+const newWord = random();
+insertWord(newWord);
+
+
 let i = 0;
-document.addEventListener ('keypress', (event) => {
-    if (event.key == word[i]) {
-        spans[i].className = "c"
+document.addEventListener('keypress', (event) => {
+    if (event.key == newWord[i]) {
+        word[i].className = "c"
         i++;
-       }
+    } else {
+        event.key == newWord[i];
+        word[i].className = "w"
+        wrongCount.textContent = ++wrongCount.textContent;
+        wordMistake.textContent = ++wordMistake.textContent;
+        i++;
+    }
+    if (randWord == length.newWord) {
+        correctCount.textContent = ++correctCount.textContent;
+        clearTimeout(timer);
+    }
 })
 
 
@@ -30,21 +49,18 @@ let min = '00';
 
 
 const timerStart = setInterval(() => {
-document.getElementById('timer').innerHTML = `${min}:${ssec}`;
+    document.getElementById('timer').innerHTML = `${min}:${ssec}`;
 
-if (ssec < 59) {
-    ssec++;
-} else {
-    min++;
-    ssec = 0;
-}
-ssec = ssec < 10 ? '0' + ssec : ssec;
-min = min < 10 ? '0' + min : min;
+    if (ssec < 59) {
+        ssec++;
+    } else {
+        min++;
+        ssec = 0;
+    }
+    ssec = ssec < 10 ? '0' + ssec : ssec;
 }, 1000);
 
 function clearTimer() {
     clearInterval(funcTimer);
     timer.textContent = `00:00`;
 }
-
-random();
